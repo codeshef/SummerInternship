66c4bf60724059b879316c25d63852154762498b
Anchal <you@example.com>
2018-05-23 12:44:19 -0400

    Adding menu functions

--- Library_System/libSys.py
+++ Library_System/libSys.py
@@ -1,5 +1,32 @@
 # Creating library management system using python
 
+
+# func to add Books
+def addBooks():
+    print("Add Books")
+
+
+# func to add members
+def addMembers():
+    print("Add Members")
+
+
+# func to Issue books
+def issueBooks():
+    print("Issue Books")
+
+
+# func  to View Books
+def viewBooks():
+    print("View Books")
+
+
+# func to View Members
+
+def viewMembers():
+    print("View Members")
+
+
 print("Welcome to our Library Management System\n")
 print("Press 1 : Add Books")
 print("Press 2 : Add Members")
@@ -10,21 +37,21 @@ print("Press 6 : Exit View\n")
 
 ch = 'y'
 
-while(ch == 'y'):
+while ch == 'y':
     choice = int(input("Enter Your choice : "))
     # print(choice)
-    if(choice==1):
-        print("Add Books")
-    elif(choice==2):
-        print("Add Members")
-    elif(choice==3):
-        print("Issue Books")
-    elif(choice==4):
-        print("View Books")
-    elif(choice==5):
-        print("View Members")
-    elif(choice==6):
+    if choice == 1:
+        addBooks()
+    elif choice == 2:
+        addMembers()
+    elif choice == 3:
+        issueBooks()
+    elif choice == 4:
+        viewBooks()
+    elif choice == 5:
+        viewMembers()
+    elif choice == 6:
         print("Exist")
+        break
     print("Do you want to continue.Press y for yes and n for no")
     ch = input('Enter y or n')
-
